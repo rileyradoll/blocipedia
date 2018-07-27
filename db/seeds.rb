51990ff4a886b24b684817d1a851b8cb8995d836
Riley Radoll <rradoll@gmail.com>
2018-07-27 01:04:48 -0400

    User story seeding data: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+require 'factory_bot_rails'
+
+10.times do
+   FactoryBot.create(:user)
+end
+
+users = User.all
+
+50.times do
+  FactoryBot.create(
+    :wiki,
+    user: users.sample
+  )
+end
+
+wikis = Wiki.all
+
+standard = FactoryBot.create(
+  :user,
+  email: 'stand@mail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: 'standard'
+)
+
+premium = FactoryBot.create(
+  :user,
+  email: 'prem@mail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: 'premium'
+)
+
+admin = FactoryBot.create(
+  :user,
+  email: 'admin@mail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: 'admin'
+)
+
+puts "Seeding complete"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
